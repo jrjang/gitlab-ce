654239ba710fc8d6c0d049a8142e3fa942cb387f
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-05-04 11:17:16 +0200

    Minor changes in note validation specs

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -30,9 +30,9 @@ FactoryGirl.define do
     author
     noteable { create(:issue, project: project) }
 
-    factory :note_on_issue,              traits: [:on_issue], aliases: [:votable_note]
     factory :note_on_commit,             traits: [:on_commit]
     factory :note_on_commit_diff,        traits: [:on_commit, :on_diff]
+    factory :note_on_issue,              traits: [:on_issue], aliases: [:votable_note]
     factory :note_on_merge_request,      traits: [:on_merge_request]
     factory :note_on_merge_request_diff, traits: [:on_merge_request, :on_diff]
     factory :note_on_project_snippet,    traits: [:on_project_snippet]
@@ -40,14 +40,20 @@ FactoryGirl.define do
     factory :downvote_note,              traits: [:award, :downvote]
     factory :upvote_note,                traits: [:award, :upvote]
 
-    trait :on_issue do
-      noteable_type 'Issue'
-    end
-
     trait :on_commit do
       noteable nil
+      noteable_type 'Commit'
+      noteable_id nil
       commit_id RepoHelpers.sample_commit.id
-      noteable_type "Commit"
+    end
+
+    trait :on_diff do
+      line_code "0_184_184"
+    end
+
+    trait :on_issue do
+      noteable_type 'Issue'
+      noteable { create(:issue, project: project) }
     end
 
     trait :on_merge_request do
@@ -61,10 +67,6 @@ FactoryGirl.define do
     trait :on_project_snippet do
       noteable_type 'Snippet'
       noteable { create(:snippet, project: project) }
-    end
-
-    trait :on_diff do
-      line_code "0_184_184"
     end
 
     trait :system do
