e55e8ef805c753a2a3b906693613e5ec9d396cde
Felipe Artur <felipefac@gmail.com>
2016-05-06 17:59:45 -0300

    Change landing page when skipping confirmation email and add documentation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -38,7 +38,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(_resource)
-    users_almost_there_path
+    _resource.confirmed_at.present? ? dashboard_projects_path : users_almost_there_path
   end
 
   def after_inactive_sign_up_path_for(_resource)
